760bae49cdf7bee8295c2772e17c756be40e8229
Yerlan Idelbayev <yerlan@MacBook-Pro-3.local>
2015-12-23 17:19:39 -0800

    simple baselines complete. dataset label names has been changed

--- prerequisits.sh
+++ prerequisits.sh
@@ -1,2 +1,2 @@
-sudo apt-get install python3-numpy python3-scipy python3-pip
+sudo apt-get install python3-numpy python3-scipy python3-pip python3-matplotlib
 sudo pip3 install nltk sklearn
--- simple_baselines.py
+++ simple_baselines.py
@@ -1,8 +1,9 @@
 import numpy as np
-from utils import visualize_representation
+
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from utils import visualize_representation
 
 import nltk
 from nltk.stem.porter import PorterStemmer
@@ -31,6 +32,7 @@ from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
 from sklearn.base import TransformerMixin
 from sklearn.grid_search import GridSearchCV
 from sklearn.cross_validation import StratifiedKFold
+from sklearn.metrics import precision_recall_fscore_support
 
 log_file = open('logs.txt', mode='w')
 
@@ -50,13 +52,11 @@ class DenseTransformer(TransformerMixin):
         return {}
 
 def experiment_1_bag_of_word():
-    pipeline_repr = Pipeline([('vectorizer', CountVectorizer(tokenizer=tokenize, max_features=1000)),
-                         ('denser', DenseTransformer())])
-    fig = visualize_representation(pipeline_repr.fit_transform(data),labels,
-                             title='Visualization of feature space of Bag of Words representation')
-
-    fig.savefig('bag_of_words_vis.png', dpi=400)
-
+    """
+    Note: excecution of this method takes a lot of time: using ec2 c4.8xlarge, results yielded
+    after 7 hours. Please refer to 'best_params.txt' to get optimal parameters for all experiments.
+    :return:
+    """
     pipeline = Pipeline([('vectorizer', CountVectorizer(tokenizer=tokenize, max_features=1000)),
                          ('denser', DenseTransformer()),
                          ('pca', PCA(n_components=0.8)),
@@ -90,14 +90,11 @@ def experiment_1_bag_of_word():
     print(scores.mean(), scores.std(), file=log_file)
 
 def experiment2_tfidf():
-    pipeline = Pipeline([('vectorizer', TfidfVectorizer(tokenizer=tokenize, max_features=1000, smooth_idf=True)),
-                         ('denser', DenseTransformer())
-                         ])
-
-    fig = visualize_representation(pipeline.fit_transform(data), labels,
-                             title='Visualiation of feature space of TfIdf representation')
-    fig.savefig('tfidf_repr.png', dpi=400)
-
+    """
+    Note: excecution of this method takes a lot of time: using ec2 c4.8xlarge, results yielded
+    after 9 hours. Please refer to 'best_params.txt' to get optimal parameters for all experiments.
+    :return:
+    """
     pipeline = Pipeline([('vectorizer', TfidfVectorizer(tokenizer=tokenize, max_features=1000)),
                          ('denser', DenseTransformer()),
                          ('pca', PCA(n_components=0.8)),
@@ -131,5 +128,79 @@ def experiment2_tfidf():
     print(scores, file=log_file)
     print(scores.mean(), scores.std(), file=log_file)
 
-experiment_1_bag_of_word()
-experiment2_tfidf()
+def visualize_experiments():
+    pipeline_repr = Pipeline([('vectorizer', CountVectorizer(tokenizer=tokenize, max_features=2000)),
+                         ('denser', DenseTransformer())])
+    fig = visualize_representation(pipeline_repr.fit_transform(data),labels,
+                             title='Visualization of feature space of Bag of Words representation')
+
+    fig.savefig('bag_of_words_vis.png', dpi=400)
+
+    pipeline = Pipeline([('vectorizer', TfidfVectorizer(tokenizer=tokenize, max_features=2000, smooth_idf=True)),
+                         ('denser', DenseTransformer())
+                         ])
+
+    fig = visualize_representation(pipeline.fit_transform(data), labels,
+                             title='Visualization of feature space of TfIdf representation')
+    fig.savefig('tfidf_repr.png', dpi=400)
+
+def statistics():
+    print('Stats for Bag of Words: Rows - precision, recall, f1, support; '
+          'Columns: environment active lifestyle physical capacity other')
+    pipeline = Pipeline([('vectorizer', CountVectorizer(tokenizer=tokenize, max_features=1000)),
+                         ('denser', DenseTransformer()),
+                         ('pca', PCA(n_components=0.8)),
+                         ('forest', RandomForestClassifier(n_estimators=10000))])
+
+    skf = StratifiedKFold(labels, 5, random_state=42, shuffle=True)
+    reports = []
+    best_params = {'forest__criterion': 'gini',
+                   'pca__n_components': 0.8,
+                   'forest__max_depth': 50,
+                   'vectorizer__max_features': 2000,
+                   'forest__min_samples_split': 4,
+                   'forest__n_estimators': 600}
+
+    for train_id, test_id in skf:
+        train_set = data[train_id]
+        pipeline.set_params(**best_params).set_params(forest__n_jobs=-1).fit(train_set,labels[train_id])
+        reports.append(precision_recall_fscore_support(labels[test_id], pipeline.predict(data[test_id]),
+              labels=['environment', 'active lifestyle', 'physical capacity', 'other']))
+
+    reports = np.array(reports)
+    print(reports.mean(axis=0))
+    print()
+
+    print('Stats for TFIDF: Rows - precision, recall, f1, support; '
+          'Columns: environment active lifestyle physical capacity other')
+
+
+    pipeline = Pipeline([('vectorizer', TfidfVectorizer(tokenizer=tokenize, max_features=1000)),
+                         ('denser', DenseTransformer()),
+                         ('pca', PCA(n_components=0.8)),
+                         ('forest', RandomForestClassifier(n_estimators=10000))])
+    skf = StratifiedKFold(labels, 5, random_state=42, shuffle=True)
+    best_params = {'forest__criterion': 'gini',
+                   'pca__n_components': 0.6,
+                   'forest__max_depth': 50,
+                   'vectorizer__max_features': 2000,
+                   'forest__min_samples_split': 2,
+                   'vectorizer__smooth_idf': True,
+                   'forest__n_estimators': 100}
+
+    report = []
+    for train_id, test_id in skf:
+        train_set = data[train_id]
+        pipeline.set_params(**best_params).set_params(forest__n_jobs=-1).fit(train_set,labels[train_id])
+        report.append(precision_recall_fscore_support(labels[test_id], pipeline.predict(data[test_id]),
+              labels=['environment', 'active lifestyle', 'physical capacity', 'other']))
+
+    report = np.array(report)
+    print(report.mean(axis=0))
+
+# Uncomment lines below to obtain best params.
+# experiment_1_bag_of_word()
+# experiment2_tfidf()
+
+#visualize_experiments()
+statistics()
