3f54245ad6fd0c5fd2312ca22f61a57c8024a884
Yerlan Idelbayev <yerlan@MacBook-Pro-3.local>
2015-12-28 06:20:22 -0800

    theano installation has been added

--- prerequisits.sh
+++ prerequisits.sh
@@ -1,5 +1,6 @@
 sudo apt-get update
 sudo apt-get install python3-numpy python3-scipy python3-pip python3-matplotlib
-sudo pip3 install https://github.com/Lasagne/Lasagne/archive/master.zip
 sudo pip3 install nltk sklearn pandas gensim
+sudo pip3 install https://github.com/Lasagne/Lasagne/archive/master.zip
+sudo pip3 install --upgrade --no-deps git+git://github.com/Theano/Theano.git
 python3 -m nltk.downloader all
