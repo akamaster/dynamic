dc38dab2c4400461417fa918977559e216c5048c
Yerlan Idelbayev <yerlan@MacBook-Pro-3.local>
2015-12-28 06:18:35 -0800

    added no x server run capability for matplotlib, prerequisites file has been updated

--- lstm_experiment.py
+++ lstm_experiment.py
@@ -1,3 +1,5 @@
+import matplotlib
+matplotlib.use('Agg')
 import lasagne
 import theano
 import theano.tensor as T
--- prerequisits.sh
+++ prerequisits.sh
@@ -1,3 +1,5 @@
 sudo apt-get update
 sudo apt-get install python3-numpy python3-scipy python3-pip python3-matplotlib
-sudo pip3 install nltk sklearn
+sudo pip3 install https://github.com/Lasagne/Lasagne/archive/master.zip
+sudo pip3 install nltk sklearn pandas gensim
+python3 -m nltk.downloader all
